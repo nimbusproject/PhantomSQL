a72a9e188342e76f7a9171efe2c7f186468327fd
BuzzTroll <buzztroll@gmail.com>
2012-05-18 14:30:30 -1000

    first commit

---
+++ phantomsql/__init__.py
@@ -0,0 +1,78 @@
+import logging
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import String, MetaData, Sequence
+from sqlalchemy import Table
+from sqlalchemy import Integer
+from sqlalchemy import types
+from sqlalchemy import Column
+from datetime import datetime
+from sqlalchemy.orm import mapper
+
+metadata = MetaData()
+
+phantom_user_pass_table = Table('phantom_user_pass', metadata,
+    Column('column_id', Integer, Sequence('launch_configuration_id_seq'), primary_key=True),
+    Column('displayname', String(128), nullable=False, unique=True),
+    Column('access_key', String(128), nullable=False, unique=True),
+    Column('access_secret', String(128), nullable=False),
+    Column('CreatedTime', types.TIMESTAMP(), default=datetime.now()),
+    )
+
+
+class PhantomUserDBObject(object):
+    def __init__(self):
+        pass
+
+mapper(PhantomUserDBObject, phantom_user_pass_table)
+
+class PhantomSQL(object):
+
+    def __init__(self, dburl):
+        self._engine = sqlalchemy.create_engine(dburl)
+        metadata.create_all(self._engine)
+        self._SessionX = sessionmaker(bind=self._engine)
+        self._Session = self._SessionX()
+
+    def _lookup_user(self, access_key):
+        q = self._Session.query(PhantomUserDBObject)
+        q = q.filter(PhantomUserDBObject.access_key==access_key)
+        db_obj = q.first()
+        return db_obj
+
+    def get_user_object_by_access_id(self, access_id):
+        db_obj = self._lookup_user(access_id)
+        if not db_obj:
+            return None
+        return dg_obj
+
+    def get_user_object_by_display_name(self, display_name):
+        q = self._Session.query(PhantomUserDBObject)
+        q = q.filter(PhantomUserDBObject.displayname==display_name)
+        db_obj = q.first()
+        if not db_obj:
+            return None
+        return db_obj
+
+    def add_alter_user(self, displayname, access_key, access_secret):
+        db_obj = self._lookup_user(access_key)
+        if not db_obj:
+            db_obj = PhantomUserDBObject()
+            db_obj.access_key = access_key
+        db_obj.access_secret = access_secret
+        db_obj.displayname = displayname
+        self._Session.add(db_obj)
+
+    def remove_user(self, access_key):
+        db_obj = self._lookup_user(access_key)
+        if not db_obj:
+            return False
+        self._Session.delete(db_obj)
+        return True
+
+    def commit(self):
+        self._Session.commit()
+
+    def add_user(self, displayname, access_id, access_secret):
+        self.add_alter_user(displayname, access_id, access_secret)
+        self.commit()
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env pythonv
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+import sys
+Version = "0.1"
+
+if float("%d.%d" % sys.version_info[:2]) < 2.6:
+    sys.stderr.write("Your Python version %d.%d.%d is not supported.\n" % sys.version_info[:3])
+    sys.stderr.write("eeagent requires Python 2.6 or newer.\n")
+    sys.exit(1)
+
+setup(name='phantomsql',
+      version=Version,
+      description='sql objects needed for pyhantom and phantomweb, broken out to decrease the dependencies of phantom web',
+      author='Nimbus Development Team',
+      author_email='workspace-user@globus.org',
+      url='http://www.nimbusproject.org/',
+      packages=find_packages(),
+      keywords = "Nimbus auto scale",
+      long_description="""Some other time""",
+      license="Apache2",
+      install_requires = ["simplejson == 2.3.2", "sqlalchemy == 0.7.6", "mysql-python >= 1.2"],
+
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Topic :: System :: Clustering',
+          'Topic :: System :: Distributed Computing',
+          ],
+     )
